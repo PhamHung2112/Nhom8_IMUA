338f008b12b849191a43d8d888075193b1f8935e
pham.hung.061020@gmail.com <pham.hung.061020@gmail.com>
2021-08-07 20:24:06 +0700

    xong phan tin noi bat

---
+++ Nhom8_IMUA/Controllers/NewsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Nhom8_IMUA.Models;
+
+namespace Nhom8_IMUA.Controllers
+{
+    public class NewsController : Controller
+    {
+        Nhom8DB db = new Nhom8DB();
+        // GET: News
+        public ActionResult Index()
+        {
+            var news = db.TinTucs.Select(p => p);
+            return View("Index", news);
+        }
+    }
+}
