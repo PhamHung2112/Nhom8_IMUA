74a8ac4d03dfa8a956ef981deb0e0b830080d2a1
vantien2000 <vantienn740@gmail.com>
2021-08-08 12:32:43 +0700

    add feature cart

--- Nhom8_IMUA/Controllers/CartController.cs
+++ Nhom8_IMUA/Controllers/CartController.cs
@@ -13,21 +13,22 @@ namespace Nhom8_IMUA.Controllers
         // GET: Cart
         public ActionResult Index()
         {
+
             return View((List<GioHang>)Session["cart"]);
         }
 
         public ActionResult AddToCart(int id, int quantity)
         {
+            List<GioHang> cart = new List<GioHang>();
             if (Session["cart"] == null)
             {
-                List<GioHang> cart = new List<GioHang>();
                 cart.Add(new GioHang { Product = db.SanPhams.Find(id), Quantity = quantity });
                 Session["cart"] = cart;
                 Session["count"] = 1;
             }
             else
             {
-                List<GioHang> cart = (List<GioHang>)Session["cart"];
+                cart = (List<GioHang>)Session["cart"];
                 //kiểm tra sản phẩm có tồn tại trong giỏ hàng chưa???
                 int index = cart.FindIndex(item=>item.Product.MaSP == id);
                 if (index != -1)
@@ -42,18 +43,18 @@ namespace Nhom8_IMUA.Controllers
                     //Tính lại số sản phẩm trong giỏ hàng
                     Session["count"] = Convert.ToInt32(Session["count"]) + 1;
                 }
-                Session["cart"] = cart;
             }
             return Json(new { Message = "Thành công", JsonRequestBehavior.AllowGet });
         }
 
+        [HttpPost]
         public ActionResult RemoveCart(int? MaSP)
         {
             List<GioHang> cart = (List<GioHang>)Session["Cart"];
             cart.RemoveAll(x => x.Product.MaSP == MaSP);
             Session["cart"] = cart;
             Session["count"] = Convert.ToInt32(Session["count"]) - 1;
-            return Json(new { Message = "Thành công", JsonRequestBehavior.AllowGet });
+            return Json(new { cart , JsonRequestBehavior.AllowGet });
         }
         
     }
