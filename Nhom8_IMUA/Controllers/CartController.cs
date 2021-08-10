b48061f2c99841db9a72617e11ffa5b3ac6327d4
vantien2000 <vantienn740@gmail.com>
2021-08-10 20:33:50 +0700

    add cart

--- Nhom8_IMUA/Controllers/CartController.cs
+++ Nhom8_IMUA/Controllers/CartController.cs
@@ -13,10 +13,48 @@ namespace Nhom8_IMUA.Controllers
         // GET: Cart
         public ActionResult Index()
         {
-            return View();
+            return View((List<GioHang>)Session["cart"]);
         }
 
+        public ActionResult AddToCart(int id, int quantity)
+        {
+            if (Session["cart"] == null)
+            {
+                List<GioHang> cart = new List<GioHang>();
+                cart.Add(new GioHang { Product = db.SanPhams.Find(id), Quantity = quantity });
+                Session["cart"] = cart;
+                Session["count"] = 1;
+            }
+            else
+            {
+                List<GioHang> cart = (List<GioHang>)Session["cart"];
+                //kiểm tra sản phẩm có tồn tại trong giỏ hàng chưa???
+                int index = cart.FindIndex(item=>item.Product.MaSP == id);
+                if (index != -1)
+                {
+                    //nếu sp tồn tại trong giỏ hàng thì cộng thêm số lượng
+                    cart[index].Quantity += quantity;
+                }
+                else
+                {
+                    //nếu không tồn tại thì thêm sản phẩm vào giỏ hàng
+                    cart.Add(new GioHang { Product = db.SanPhams.Find(id), Quantity = quantity });
+                    //Tính lại số sản phẩm trong giỏ hàng
+                    Session["count"] = Convert.ToInt32(Session["count"]) + 1;
+                }
+                Session["cart"] = cart;
+            }
+            return Json(new { Message = "Thành công", JsonRequestBehavior.AllowGet });
+        }
 
+        public ActionResult RemoveCart(int? MaSP)
+        {
+            List<GioHang> cart = (List<GioHang>)Session["Cart"];
+            cart.RemoveAll(x => x.Product.MaSP == MaSP);
+            Session["cart"] = cart;
+            Session["count"] = Convert.ToInt32(Session["count"]) - 1;
+            return Json(new { Message = "Thành công", JsonRequestBehavior.AllowGet });
+        }
         
     }
 }
