2e08782d0cca71a571bfbcd2453e22c14807e0e7
vantien2000 <vantienn740@gmail.com>
2021-08-13 23:54:18 +0700

    add customer admin

---
+++ Nhom8_IMUA/Areas/Admin/Controllers/CustomerController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Nhom8_IMUA.Models;
+using PagedList;
+
+namespace Nhom8_IMUA.Areas.Admin.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private Nhom8DB db = new Nhom8DB();
+
+        // GET: Admin/Customer
+        public ActionResult Index(int? page)
+        {
+            var custom = db.NguoiDungs.Where(x => x.Loai == false).Select(x=>x);
+            custom = custom.OrderBy(c => c.MaND);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(custom.ToPagedList(pageNumber, pageSize));
+        }
+
+        
+        // GET: Admin/Customer/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NguoiDung nguoiDung = db.NguoiDungs.Find(id);
+            if (nguoiDung == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nguoiDung);
+        }
+
+        // POST: Admin/Customer/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(NguoiDung nguoiDung)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nguoiDung).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nguoiDung);
+        }
+
+        
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
--- Nhom8_IMUA/Models/NguoiDung.cs
+++ Nhom8_IMUA/Models/NguoiDung.cs
@@ -1,7 +1,8 @@
-namespace Nhom8_IMUA.Models
+﻿namespace Nhom8_IMUA.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
@@ -16,37 +17,50 @@ namespace Nhom8_IMUA.Models
         }
 
         [Key]
+        [DisplayName("Mã người dùng")]
+        [Required(ErrorMessage="Mã người dùng không được để trống")]
         public int MaND { get; set; }
 
-        [Required]
+        [DisplayName("Tên đăng nhập")]
+        [Required(ErrorMessage = "Tên đăng nhập không được để trống")]
         [StringLength(50)]
         public string TenDangNhap { get; set; }
 
-        [Required]
+        [DisplayName("Mật khẩu")]
+        [Required(ErrorMessage = "Mật khẩu dùng không được để trống")]
         [StringLength(50)]
         public string MatKhau { get; set; }
 
-        [Required]
+        [DisplayName("Họ tên")]
+        [Required(ErrorMessage = "Họ tên dùng không được để trống")]
         [StringLength(100)]
         public string HoTen { get; set; }
 
+        [DisplayName("Ảnh đại diện")]
         [Column(TypeName = "text")]
         public string AnhDaiDien { get; set; }
 
-        [Required]
+        [DisplayName("Số điện thoại")]
+        [Required(ErrorMessage = "Số điện thoại không được để trống")]
         [StringLength(50)]
         public string SoDT { get; set; }
 
         [Column(TypeName = "ntext")]
-        [Required]
+        [DisplayName("Địa chỉ")]
+        [Required(ErrorMessage = "Địa chỉ không được để trống")]
         public string DiaChi { get; set; }
 
-        [Required]
+        [DisplayName("Email")]
+        [Required(ErrorMessage = "Email không được để trống")]
         [StringLength(50)]
         public string Email { get; set; }
 
+        [DisplayName("Loại tai khoản")]
+        [Required(ErrorMessage = "Loại tai khoản không được để trống")]
         public bool Loai { get; set; }
 
+        [DisplayName("Trạng thái")]
+        [Required(ErrorMessage = "Trạng thái khoản không được để trống")]
         public bool TrangThai { get; set; }
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
