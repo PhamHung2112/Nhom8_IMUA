f8b2baedb5a9fae5a2aba8ffcd18d6530ebfa7d8
unknown <duong9x2000@gmail.com>
2021-08-13 23:06:04 +0700

    Data SanPham + phan trang cho Admin

---
+++ Nhom8_IMUA/Areas/Admin/Controllers/AdminHomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using Nhom8_IMUA.Models;
+
+namespace Nhom8_IMUA.Areas.Admin.Controllers
+{
+    public class AdminHomeController : Controller
+    {
+        // GET: Admin/Home
+        private Nhom8DB db = new Nhom8DB();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SanPham(int? page)
+        {
+            var sanPham = db.SanPhams.Select(x => x);
+            sanPham = sanPham.OrderBy(s => s.MaSP);
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return PartialView(sanPham.ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
