a9b64e6a5ff6be6dec76e644114c62471b1697ce
pham.hung.061020@gmail.com <pham.hung.061020@gmail.com>
2021-08-14 22:32:17 +0700

    fix conflict

--- Nhom8_IMUA/Areas/Admin/Controllers/CustomerController.cs
+++ Nhom8_IMUA/Areas/Admin/Controllers/CustomerController.cs
@@ -18,9 +18,6 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
         // GET: Admin/Customer
         public ActionResult Index(string sortOrder, string searchString, string currentFilter, int? page)
         {
-<<<<<<< HEAD
-            var custom = db.NguoiDungs.Select(x=>x);
-=======
             ViewBag.CurrentSort = sortOrder;
             ViewBag.SapTheoID = String.IsNullOrEmpty(sortOrder) ? "ten_desc" : "";
             if (searchString != null)
@@ -34,7 +31,7 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
             ViewBag.CurrentFilter = searchString;
 
             var custom = db.NguoiDungs.Where(x => x.Loai == false).Select(x => x);
->>>>>>> origin/duong
+
             custom = custom.OrderBy(c => c.MaND);
             if (!String.IsNullOrEmpty(searchString)) // kiểm tra chuỗi tìm kiếm có rỗng/null hay không
             {
@@ -55,30 +52,11 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
             int pageNumber = (page ?? 1);
             return View(custom.ToPagedList(pageNumber, pageSize));
         }
-<<<<<<< HEAD
-        
-=======
 
->>>>>>> origin/duong
         // GET: Admin/Customer/Edit/5
         public ActionResult ChangeStatus(int? id)
         {
             Response.Write("<script>alert(" + id + ");</script>");
-<<<<<<< HEAD
-            if (id==null)
-            {
-                return RedirectToAction("Index");
-            }
-            NguoiDung custom = db.NguoiDungs.Find(id);
-            if (custom.TrangThai.Equals(false))
-            {
-                custom.TrangThai = true;
-            }
-            else
-            {
-                custom.TrangThai = false;
-            }
-=======
             if (id == null)
             {
                 return RedirectToAction("Index");
@@ -92,14 +70,9 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
             {
                 custom.TrangThai = false;
             }
->>>>>>> origin/duong
             db.Entry(custom).State = EntityState.Modified;
             db.SaveChanges();
             return RedirectToAction("Index");
         }
-<<<<<<< HEAD
-
-=======
->>>>>>> origin/duong
     }
 }
