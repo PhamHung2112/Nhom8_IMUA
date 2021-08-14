bd654c9692c38d5da9f6dcbc09edf6eb483042e8
unknown <duong9x2000@gmail.com>
2021-08-14 21:55:35 +0700

    Fix css search, fix css giao dien, add search cho customer

--- Nhom8_IMUA/Areas/Admin/Controllers/CustomerController.cs
+++ Nhom8_IMUA/Areas/Admin/Controllers/CustomerController.cs
@@ -16,55 +16,62 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
         private Nhom8DB db = new Nhom8DB();
 
         // GET: Admin/Customer
-        public ActionResult Index(int? page)
+        public ActionResult Index(string sortOrder, string searchString, string currentFilter, int? page)
         {
-            var custom = db.NguoiDungs.Where(x => x.Loai == false).Select(x=>x);
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.SapTheoID = String.IsNullOrEmpty(sortOrder) ? "ten_desc" : "";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+
+            var custom = db.NguoiDungs.Where(x => x.Loai == false).Select(x => x);
             custom = custom.OrderBy(c => c.MaND);
-            int pageSize = 10;
+            if (!String.IsNullOrEmpty(searchString)) // kiểm tra chuỗi tìm kiếm có rỗng/null hay không
+            {
+                custom = custom.Where(p => p.HoTen.Contains(searchString)); //lọc theo chuỗi tìm kiếm
+            }
+
+            switch (sortOrder)
+            {
+                case "ten_desc":
+                    custom = custom.OrderByDescending(s => s.MaND);
+                    break;
+                default:
+                    custom = custom.OrderBy(s => s.MaND);
+                    break;
+            }
+            ViewData["Count"] = custom.Count().ToString();
+            int pageSize = 5;
             int pageNumber = (page ?? 1);
             return View(custom.ToPagedList(pageNumber, pageSize));
         }
 
-        
         // GET: Admin/Customer/Edit/5
-        public ActionResult Edit(int? id)
+        public ActionResult ChangeStatus(int? id)
         {
+            Response.Write("<script>alert(" + id + ");</script>");
             if (id == null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            NguoiDung nguoiDung = db.NguoiDungs.Find(id);
-            if (nguoiDung == null)
-            {
-                return HttpNotFound();
-            }
-            return View(nguoiDung);
-        }
-
-        // POST: Admin/Customer/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(NguoiDung nguoiDung)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(nguoiDung).State = EntityState.Modified;
-                db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(nguoiDung);
-        }
-
-        
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
+            NguoiDung custom = db.NguoiDungs.Find(id);
+            if (custom.TrangThai.Equals(false))
             {
-                db.Dispose();
+                custom.TrangThai = true;
             }
-            base.Dispose(disposing);
+            else
+            {
+                custom.TrangThai = false;
+            }
+            db.Entry(custom).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
