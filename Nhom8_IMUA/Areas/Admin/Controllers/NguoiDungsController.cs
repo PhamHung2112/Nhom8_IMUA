c182abb212805472c09d75cf0bc527a77f50741a
pham.hung.061020@gmail.com <pham.hung.061020@gmail.com>
2021-08-17 13:57:15 +0700

    push code

--- Nhom8_IMUA/Areas/Admin/Controllers/NguoiDungsController.cs
+++ Nhom8_IMUA/Areas/Admin/Controllers/NguoiDungsController.cs
@@ -178,5 +178,97 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
             Session[CommonConstants.SESSION_CREDENTIALS] = null;
             return Redirect("/");
         }
+
+        public ActionResult ChangeInfor(int id)
+        {
+            var user = new UserDAO().ViewDetails(id);
+            return View(user);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult ChangeInfor(NguoiDung nguoiDung)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    nguoiDung.AnhDaiDien = "";
+
+                    var backupImg = Request["Image1"];
+                    var f = Request.Files["ImageFile"];
+
+                    if (f != null && f.ContentLength > 0)
+                    {
+                        string FileName = System.IO.Path.GetFileName(f.FileName);
+                        string UploadPath = Server.MapPath("~/assets/Images/AnhDaiDien/" + FileName);
+                        f.SaveAs(UploadPath);
+                        nguoiDung.AnhDaiDien = FileName;
+                    }
+                    else
+                    {
+                        nguoiDung.AnhDaiDien = backupImg;
+                    }
+                    var userSession = new UserLogin();
+                    var user = db.NguoiDungs.Find(nguoiDung.MaND);
+                    user.HoTen = nguoiDung.HoTen;
+                    user.SoDT = nguoiDung.SoDT;
+                    user.Email = nguoiDung.Email;
+                    user.DiaChi = nguoiDung.DiaChi;
+                    user.AnhDaiDien = nguoiDung.AnhDaiDien;
+                    db.SaveChanges();
+                    userSession.UserID = user.MaND;
+                    userSession.HoTen = user.HoTen;
+                    userSession.AnhDaiDien = user.AnhDaiDien;
+                    userSession.GroupID = user.GroupID;
+                    Session.Add(CommonConstants.USER_SESSION, userSession);
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Thay đổi thông tin thất bại");
+                }
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error(ex.Message);
+            }
+            return View(nguoiDung);
+        }
+
+        [HttpGet]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ChangePassword(string oldpass, string newpass)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var session = (Nhom8_IMUA.Common.UserLogin)Session[Nhom8_IMUA.Common.CommonConstants.USER_SESSION];
+                if (session != null)
+                {
+                    if (session.Password != Encryptor.MD5Hash(oldpass))
+                    {
+                        ModelState.AddModelError("", "Mật khẩu cũ không chính xác");
+                    }
+                    else
+                    {
+                        NguoiDung edit = db.NguoiDungs.Where(p => p.MaND == session.UserID).FirstOrDefault();
+                        edit.MatKhau = Encryptor.MD5Hash(newpass);
+                        db.SaveChanges();
+                        session.Password = Encryptor.MD5Hash(newpass);
+                        ViewBag.Success = "Thay đổi mật khẩu thành công";
+                    }
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Đổi mật khẩu thất bại");
+            }
+            return View();
+        }
     }
 }
