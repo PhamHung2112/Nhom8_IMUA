bc9636f55655c11eda16a109719b451ba4b1d7f2
vantien2000 <vantienn740@gmail.com>
2021-08-17 18:11:51 +0700

    add  tin tức

--- Nhom8_IMUA/Areas/Admin/Controllers/TinTucsController.cs
+++ Nhom8_IMUA/Areas/Admin/Controllers/TinTucsController.cs
@@ -45,11 +45,21 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [ValidateInput(false)]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "MaTinTuc,TieuDe,TomTat,NoiDung,AnhTinTuc")] TinTuc tinTuc)
         {
             if (ModelState.IsValid)
             {
+                tinTuc.AnhTinTuc = "";
+                var f = Request.Files["AnhTinTuc"];
+                if (f != null && f.ContentLength > 0)
+                {
+                    string FileName = System.IO.Path.GetFileName(f.FileName);
+                    string UploadPath = Server.MapPath("~/assets/Images/TinTuc/" + FileName);
+                    f.SaveAs(UploadPath);
+                    tinTuc.AnhTinTuc = FileName;
+                }
                 db.TinTucs.Add(tinTuc);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -77,11 +87,23 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
+        [ValidateInput(false)]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "MaTinTuc,TieuDe,TomTat,NoiDung,AnhTinTuc")] TinTuc tinTuc)
         {
+
             if (ModelState.IsValid)
             {
+                //tinTuc.AnhTinTuc = "";
+                //var anh = Request["AnhTT"];
+                var f = Request.Files["AnhTinTuc"];
+                if (f != null && f.ContentLength > 0)
+                {
+                    string FileName = System.IO.Path.GetFileName(f.FileName);
+                    string UploadPath = Server.MapPath("~/assets/Images/TinTuc/" + FileName);
+                    f.SaveAs(UploadPath);
+                    tinTuc.AnhTinTuc = FileName;
+                }
                 db.Entry(tinTuc).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -90,29 +112,28 @@ namespace Nhom8_IMUA.Areas.Admin.Controllers
         }
 
         // GET: Admin/TinTucs/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            TinTuc tinTuc = db.TinTucs.Find(id);
-            if (tinTuc == null)
-            {
-                return HttpNotFound();
-            }
-            return View(tinTuc);
-        }
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    TinTuc tinTuc = db.TinTucs.Find(id);
+        //    if (tinTuc == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(tinTuc);
+        //}
 
         // POST: Admin/TinTucs/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
+        [HttpPost]
+        public JsonResult Delete(int id)
         {
             TinTuc tinTuc = db.TinTucs.Find(id);
             db.TinTucs.Remove(tinTuc);
             db.SaveChanges();
-            return RedirectToAction("Index");
+            return Json(new { ThongBao = "successs" });
         }
 
         protected override void Dispose(bool disposing)
---
+++ Nhom8_IMUA/Areas/Admin/assets/js/delete.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $(".delBtn").click(function (e) {
+        e.preventDefault();
+        var result = confirm("Bạn muốn xóa tin tức này ?");
+        if (result) {
+            $.ajax({
+                url: "/Admin/TinTucs/Delete",
+                type: "POST",
+                data: {
+                    id: $(this).data("id")
+                },
+                dataType: "json",
+                success: function (data) {
+                    if (data != null) {
+                        window.location.reload();
+                    } else {
+                        alert("loi");
+                    }
+                },
+                error: function () {
+                    alert("Lỗi trong khi xóa!");
+                }
+            });
+        }
+    })
+});
